545a644814fde89b0b164e04b0bd985a8f7fd115
AP Solanki <88495030+ateendra24@users.noreply.github.com>
2024-09-15 16:59:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Todo App with React & Local Storage
+
+This is a simple and efficient Todo app built using React, where tasks are stored locally in the browser using Local Storage. This ensures that your tasks persist even after refreshing or closing the app.
+
+## Features
+
+- **Add Tasks**: Add new tasks with ease.
+- **Edit Tasks**: Modify existing tasks.
+- **Delete Tasks**: Remove tasks when completed or no longer needed.
+- **Local Storage**: Keeps tasks saved in your browser's local storage, ensuring persistence across sessions.
+- **Responsive Design**: The app is styled using Tailwind CSS to ensure it works across all screen sizes.
+
+## Tech Stack
+
+- **React**: For building the user interface.
+- **Vite**: For fast development and building the app.
+- **Tailwind CSS**: For modern and responsive styling.
+- **Local Storage**: For saving data persistently in the browser.
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/username/React-Todo-App.git
+   cd 10todocontextlocal
+
+2. Install dependencies:
+    ```bash
+    npm install
+
+3. Run the app in development mode:
+    ```bash
+    npm run dev
+
+Open http://localhost:5173 to view it in your browser.
+
+
+## Contributing
+Feel free to contribute to the project by opening issues or submitting pull requests.
+
+## License
+This project is licensed under the MIT License.
+
+## Contact
+If you have any questions or suggestions regarding the project, feel free to reach out!
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
+function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
+  return (
+    <div
+      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+        todo.completed ? "bg-[#797979]" : "bg-[#bfbfbf]"
+      }`}
+    >
+      <input
+        type="checkbox"
+        className="cursor-pointer"
+        checked={todo.completed}
+        onChange={toggleCompleted}
+      />
+      <input
+        type="text"
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+        } ${todo.completed ? "line-through" : ""}`}
+        value={todoMsg}
+        onChange={(e) => setTodoMsg(e.target.value)}
+        readOnly={!isTodoEditable}
+      />
+      <button
+        className="inline-flex w-8 h-8 rounded-lg text-sm justify-center items-center shrink-0 disabled:opacity-50"
+        onClick={() => {
+          if (todo.completed) return;
+
+          if (isTodoEditable) {
+            editTodo();
+          } else setIsTodoEditable((prev) => !prev);
+        }}
+        disabled={todo.completed}
+      >
+        {isTodoEditable ? <svg data-id="18" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path><path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path><path d="M7 3v4a1 1 0 0 0 1 1h7"></path></svg> : <svg data-id="16" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M12 22h6a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v10"></path><path d="M14 2v4a2 2 0 0 0 2 2h4"></path><path d="M10.4 12.6a2 2 0 1 1 3 3L8 21l-4 1 1-4Z"></path></svg>}
+      </button>
+      <button
+        className="inline-flex w-8 h-8  text-sm  justify-center items-center shrink-0"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        <svg data-id="14" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path></svg>
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
